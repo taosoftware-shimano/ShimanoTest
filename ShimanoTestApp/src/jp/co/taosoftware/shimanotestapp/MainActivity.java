6e7e217693f976f16df4b5e1c587c053caef7e65
Eiji Shimano <shimano@taosoftware.co.jp>
2012-09-27 16:44:32 +0900

    コンフリクトするかな

--- ShimanoTestApp/src/jp/co/taosoftware/shimanotestapp/MainActivity.java
+++ ShimanoTestApp/src/jp/co/taosoftware/shimanotestapp/MainActivity.java
@@ -2,10 +2,13 @@ package jp.co.taosoftware.shimanotestapp;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 
 import android.os.Bundle;
 import android.os.Handler;
@@ -23,7 +26,18 @@ public class MainActivity extends Activity {
 	
 	Handler mHandler = new Handler();
 	TextView mTextView;
-	String[][] mData = {{"山田", "太郎"}, {"鈴木", "花子"},{"島野", "英司"}};
+	String[][] mData = {
+			{"山田", "太郎01"}, {"鈴木", "花子02"},{"田中", "次郎03"},{"佐々木", "さち子04"},{"島野", "英司05"},
+			{"山田", "太郎06"}, {"鈴木", "花子07"},{"田中", "次郎08"},{"佐々木", "さち子09"},{"島野", "英司10"},
+			{"山田", "太郎11"}, {"鈴木", "花子12"},{"田中", "次郎13"},{"佐々木", "さち子14"},{"島野", "英司15"},
+			{"山田", "太郎16"}, {"鈴木", "花子17"},{"田中", "次郎18"},{"佐々木", "さち子19"},{"島野", "英司20"},
+			{"山田", "太郎21"}, {"鈴木", "花子22"},{"田中", "次郎23"},{"佐々木", "さち子24"},{"島野", "英司25"},
+			{"山田", "太郎26"}, {"鈴木", "花子27"},{"田中", "次郎28"},{"佐々木", "さち子29"},{"島野", "英司30"},
+			{"山田", "太郎31"}, {"鈴木", "花子32"},{"田中", "次郎33"},{"佐々木", "さち子34"},{"島野", "英司35"},
+			{"山田", "太郎36"}, {"鈴木", "花子37"},{"田中", "次郎38"},{"佐々木", "さち子39"},{"島野", "英司40"},
+			{"山田", "太郎41"}, {"鈴木", "花子42"},{"田中", "次郎43"},{"佐々木", "さち子44"},{"島野", "英司45"},
+			{"山田", "太郎46"}, {"鈴木", "花子57"},{"田中", "次郎48"},{"佐々木", "さち子49"},{"島野", "英司50"}
+			};
 	private static final String FILE_NAME = "sample.json";
 
     @Override
@@ -77,6 +91,7 @@ public class MainActivity extends Activity {
            					jg.writeStringField("lastName", mData[i][0]); 
            					jg.writeStringField("firstName", mData[i][1]);
            					jg.writeEndObject();
+           					Thread.sleep(1000);	//休んでみる
             			}
             		}
             		jg.writeEndObject();
@@ -105,14 +120,46 @@ public class MainActivity extends Activity {
     private void readJsonData() {
     	// スレッド起動
         (new Thread(new Runnable() {
-        	String message = "よんだよ";
+        	String message = "";
             @Override
             public void run() {
+            	ArrayList<Person> personList;
                 // JasonDataを読み込む
             	try {
             		FileInputStream fis = openFileInput(FILE_NAME);
-            		//ObjectMapper mapper = ObjectMapper();
+            		JsonFactory jsonFactory = new JsonFactory();  
+            		JsonParser jp = jsonFactory.createJsonParser(fis); 
             		
+            		//コレクションの作製
+            		personList = new ArrayList<Person>();  
+            		
+            		jp.nextToken(); //Start Object
+            		while (jp.nextToken() != JsonToken.END_OBJECT) {
+                		String fieldName = jp.getCurrentName();
+                		if ("Person".equals(fieldName)){
+                			Person psn = new Person();
+                			//1人分のデータ
+                			jp.nextToken(); //StartObject
+                			while (jp.nextToken() != JsonToken.END_OBJECT){
+                				String itemName = jp.getCurrentName();
+                				if ("lastName".equals(itemName)) {
+                					psn.lastName = jp.getText();
+                				} else {
+                					if ("firstName".equals(itemName)){
+                						psn.firstName = jp.getText();     
+                					}
+                				}
+                			}
+                			//1人分のデータ出来上がり＆格納
+                			personList.add(psn);
+                		}
+        			}
+            		//画面表示用文言の編集
+                	 Iterator<Person> itr = personList.iterator();
+                	 while (itr.hasNext() == true) {
+                		 Person psn = itr.next();
+                		 message += "姓=" + psn.lastName + "  " + "名=" + psn.firstName + "\n";
+                	 }
             	} catch (Exception ex) {
             		Log.e("Shimano-Test", "File Read Error!!");
             		message = String.valueOf("読み込みエラー");
